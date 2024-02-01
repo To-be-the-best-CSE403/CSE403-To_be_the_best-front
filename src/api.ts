15e95b1ae93b482a99f75f9a06a0534ceef95d8e
PopoDev <67870588+PopoDev@users.noreply.github.com>
2024-01-31 22:49:48 -0800

    Add API Request for Teambuilder (#13)

---
+++ src/api.ts
@@ -0,0 +1,7 @@
+// API endpoints
+const API_BASE_URL = 'https://tobethebest.vercel.app/api';
+const API_TEST_URL = `${API_BASE_URL}/test`;
+export const API_ENDPOINTS = {
+  TEAMBUILDER: `${API_BASE_URL}/teambuilder`,
+  TEAMBUILDER_TEST: `${API_TEST_URL}/teambuilder`,
+};
