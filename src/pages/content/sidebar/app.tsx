a9109e6b9ac886d9e3c4714e804ccc0a3cdb4dde
PopoDev <popocompte@gmail.com>
2024-02-02 10:17:33 -0800

    Add Extension Sidebar Test

--- src/pages/content/sidebar/app.tsx
+++ src/pages/content/sidebar/app.tsx
@@ -13,19 +13,19 @@ export default function App() {
 
   return (
     <>
-      <div id="tobethebest-toggle" className={toggled ? '' : 'active'} onMouseEnter={handleToggle}>
+      <div id="tobethebest-toggle" data-testid="test-tobethebest-toggle" className={toggled ? '' : 'active'} onMouseEnter={handleToggle}>
         <p id="tobethebest-toggle__text">ToBeTheBest</p>
       </div>
-      <div id="tobethebest-sidebar" className={toggled ? 'active' : ''} onMouseLeave={handleToggle}>
-        <div id="tobethebest-sidebar__container">
-            <div id="tobethebest-sidebar-header" className="tobethebest-sidebar__component">
+      <div id="tobethebest-sidebar" data-testid="test-tobethebest-sidebar" className={toggled ? 'active' : ''} onMouseLeave={handleToggle}>
+        <div id="tobethebest-sidebar__container" data-testid="test-tobethebest-sidebar__container">
+            <div id="tobethebest-sidebar-header" data-testid="test-tobethebest-sidebar-header" className="tobethebest-sidebar__component">
                 <h1>ToBeTheBest</h1>
             </div>
-            <div id="tobethebest-sidebar-teambuilder" className="tobethebest-sidebar__component">
+            <div id="tobethebest-sidebar-teambuilder" data-testid="test-tobethebest-sidebar-teambuilder" className="tobethebest-sidebar__component">
                 <h2>Team Builder</h2>
                 <button id="tobethebest-teambuilder-button">Create Team</button>
             </div>
-            <div id="tobethebest-sidebar-resources" className="tobethebest-sidebar__component">
+            <div id="tobethebest-sidebar-resources" data-testid="test-tobethebest-sidebar-resources" className="tobethebest-sidebar__component">
                 <h2>Resources</h2>
                 <button id="tobethebest-website-button">Website</button>
                 <button id="tobethebest-wiki-button">Wiki</button>
