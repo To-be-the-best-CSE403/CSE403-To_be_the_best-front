64cf4ed83fed66e37cbece3548a1bbc60764248e
PopoDev <popocompte@gmail.com>
2024-02-05 22:47:26 -0800

    Add Team creation and saving

--- src/pages/content/injected/app.tsx
+++ src/pages/content/injected/app.tsx
@@ -1,15 +1,37 @@
-
+import { API_ENDPOINTS } from '@root/src/api';
 import { useEffect } from 'react';
+import { Teams } from '@pkmn/sim';
 
 export default function App() {
   useEffect(() => {
     console.log('tobethebest teampane injected');
   }, []);
 
+  const doCreateTeamClick = () => {
+    fetch(API_ENDPOINTS.TEAMBUILDER_TEST)
+      .then(response => response.json())
+      .then(data => {
+        console.log('[ToBeTheBest] Team created:', data);
+
+        const teamName = 'gen9ou]ToBeTheBest';
+        const teamPacked = Teams.pack(data);
+        const team = teamName.concat('|', teamPacked);
+        console.log('[ToBeTheBest] Team packed:', team);
+
+        const showdownTeams = localStorage.getItem('showdown_teams');
+        localStorage.setItem('showdown_teams', showdownTeams ? showdownTeams.concat('\n', team) : team);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
     <div id="tobethebest-teampane-container">
       <h2>ToBeTheBest</h2>
-      <button className='big button'>Create Best Team</button>
-    </div>    
-  )
+      <button className="big button" onClick={doCreateTeamClick}>
+        Create Best Team
+      </button>
+    </div>
+  );
 }
