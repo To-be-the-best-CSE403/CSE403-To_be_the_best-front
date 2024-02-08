a0fed6a65bc2d9e98667b6e889759079d9a46765
PopoDev <67870588+PopoDev@users.noreply.github.com>
2024-02-08 11:56:09 -0800

    Improve UI Styling Popup and Sidebar (#38)

--- src/pages/content/sidebar/app.tsx
+++ src/pages/content/sidebar/app.tsx
@@ -9,30 +9,65 @@ export default function App() {
 
   const handleToggle = () => {
     setToggled(!toggled);
-  }
+  };
 
   return (
     <>
-      <div id="tobethebest-toggle" data-testid="test-tobethebest-toggle" className={toggled ? '' : 'active'} onMouseEnter={handleToggle}>
+      <div
+        id="tobethebest-toggle"
+        data-testid="test-tobethebest-toggle"
+        className={toggled ? '' : 'active'}
+        onMouseEnter={handleToggle}>
         <p id="tobethebest-toggle__text">ToBeTheBest</p>
       </div>
-      <div id="tobethebest-sidebar" data-testid="test-tobethebest-sidebar" className={toggled ? 'active' : ''} onMouseLeave={handleToggle}>
+      <div
+        id="tobethebest-sidebar"
+        data-testid="test-tobethebest-sidebar"
+        className={toggled ? 'active' : ''}
+        onMouseLeave={handleToggle}>
         <div id="tobethebest-sidebar__container" data-testid="test-tobethebest-sidebar__container">
-            <div id="tobethebest-sidebar-header" data-testid="test-tobethebest-sidebar-header" className="tobethebest-sidebar__component">
-                <h1>ToBeTheBest</h1>
-            </div>
-            <div id="tobethebest-sidebar-teambuilder" data-testid="test-tobethebest-sidebar-teambuilder" className="tobethebest-sidebar__component">
-                <h2>Team Builder</h2>
-                <button id="tobethebest-teambuilder-button">Create Team</button>
-            </div>
-            <div id="tobethebest-sidebar-resources" data-testid="test-tobethebest-sidebar-resources" className="tobethebest-sidebar__component">
-                <h2>Resources</h2>
-                <button id="tobethebest-website-button">Website</button>
-                <button id="tobethebest-wiki-button">Wiki</button>
-                <button id="tobethebest-github-button">Github</button>
-            </div>
+          <div
+            id="tobethebest-sidebar-header"
+            data-testid="test-tobethebest-sidebar-header"
+            className="tobethebest-sidebar__component">
+            <h1>ToBeTheBest</h1>
+          </div>
+          <div
+            id="tobethebest-sidebar-teambuilder"
+            data-testid="test-tobethebest-sidebar-teambuilder"
+            className="tobethebest-sidebar__component">
+            <h2>Team Builder</h2>
+            <button
+              id="tobethebest-teambuilder-button"
+              onClick={() => (window.location.href = 'https://play.pokemonshowdown.com/teambuilder')}>
+              Create Team
+            </button>
+          </div>
+          <div
+            id="tobethebest-sidebar-resources"
+            data-testid="test-tobethebest-sidebar-resources"
+            className="tobethebest-sidebar__component">
+            <h2>Resources</h2>
+            <button
+              id="tobethebest-website-button"
+              onClick={() => window.open('https://tobethebest.vercel.app', '_blank')}>
+              Website
+            </button>
+            <button
+              id="tobethebest-wiki-button"
+              onClick={() => window.open('https://tobethebest.vercel.app/dashboard/getting-started', '_blank')}>
+              Wiki
+            </button>
+            <button
+              id="tobethebest-github-button"
+              onClick={() =>
+                window.open('https://github.com/To-be-the-best-CSE403/CSE403-To_be_the_best-front', '_blank')
+              }>
+              GitHub
+            </button>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
